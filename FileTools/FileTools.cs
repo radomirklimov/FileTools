24f5264a72cc9622ea2c21b6ce40bbe539d93601
Radomyr Klymov <radomyr.klymov@s-atiw.de>
2026-02-20 13:19:55 +0100

    all

---
+++ FileTools/FileTools.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTools
+{
+    internal class FileTools
+    {
+        private string ConsoleLikeOutput(Dictionary<string, string> infos)
+        {
+            return $"Verzeichnis von {infos["Full Path"]}<br>";
+        }
+
+        static public string FileList(string fullFilePath)
+        {
+            bool isFile = File.Exists(fullFilePath);
+            FileInfo fileInfo = new FileInfo(fullFilePath);
+
+            Dictionary<string, string> infos = new Dictionary<string, string>();
+            infos.Add("Full Path", fullFilePath);
+            infos.Add("Name", fileInfo.Name);
+            infos.Add("Extention", fileInfo.Extension);
+            infos.Add("Mode", fileInfo.UnixFileMode.ToString());
+            infos.Add("Last write time", fileInfo.LastWriteTime.ToString());
+            infos.Add("Creation time", fileInfo.CreationTime.ToString());
+
+            if (isFile)
+            {
+                infos.Add("Lenght", fileInfo.Length.ToString());
+            } else
+            {
+                infos.Add("Length", "Dir");
+            }
+
+            return new FileTools().ConsoleLikeOutput(infos);
+        }
+
+    }
+}
---
+++ FileTools/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FileTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> file1 = FileTools.FileList(@"C:\Users\radomyr.klymov\Downloads\CD-Manager-main.zip");
+
+            Console.WriteLine(string.Join(Environment.NewLine, file1));
+        }
+    }
+}
